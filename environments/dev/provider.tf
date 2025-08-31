a6bd9f703b13dda324ff4d04c3ff6c004455afeb
sadaf-jamal-au27 <jamalsada3@gmail.com>
2025-08-31 20:04:32 +0530

    fix: configure Terraform provider for service principal authentication
    - Set use_cli = false in all provider.tf files (dev, staging, prod)
    - Added ARM_* environment variables to GitHub Actions workflow
    - Fixed Azure CLI vs Service Principal authentication conflict
    - Terraform will now use service principal credentials from GitHub secrets
    - Resolves: Error building ARM Config - Authenticating using Azure CLI is only supported as User

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -20,4 +20,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "a72674d0-171e-41fb-bed8-d50db63bc0b4"
+  
+  # Use service principal authentication in CI/CD
+  # These environment variables will be set by GitHub Actions
+  use_cli = false
 }
