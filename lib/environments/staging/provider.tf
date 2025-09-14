a59805fcb636ad55de62aa80716f82e61ee9d000
sadaf-jamal-au27 <jamalsada3@gmail.com>
2025-09-14 10:10:19 +0530

    Enable Azure CLI authentication for all environments
    - Changed use_cli from false to true in all provider configurations
    - Resolves 'unable to build authorizer for Resource Manager API' error
    - Enables Terraform to use Azure CLI authentication
    - All environments (dev, staging, prod) now properly authenticated
    - Terraform plans now working successfully for both dev and staging

--- lib/environments/staging/provider.tf
+++ lib/environments/staging/provider.tf
@@ -23,5 +23,5 @@ provider "azurerm" {
   features {}
   subscription_id = "8cbf7ca1-02c5-4b17-aa60-0a669dc6f870"
   tenant_id       = "0fc07ff0-d314-4e80-aed7-2dddffabbec7"
-  use_cli         = false
+  use_cli         = true
 }
